3fec4a61c7db66119453cf590ae051a0b74c14e8
lyyyd <David.Jackson.Lyd@gmail.com>
2023-05-13 13:09:00 +0800

    links-core pub

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: lyyyd David.Jackson.Lyd@gmail.com
+ * @Date: 2023-05-13 09:41:30
+ * @LastEditors: lyyyd David.Jackson.Lyd@gmail.com
+ * @LastEditTime: 2023-05-13 13:08:50
+ * @FilePath: \links-core\src\index.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 export { PicGo } from './core/PicGo'
 export { Lifecycle } from './core/Lifecycle'
 
@@ -7,6 +17,6 @@ export { LifecyclePlugins } from './lib/LifecyclePlugins'
 export { Commander } from './lib/Commander'
 export { PluginLoader } from './lib/PluginLoader'
 export { Request } from './lib/Request'
-export * as PicGoUtils from './utils/common'
+// export * as PicGoUtils from './utils/common'
 
 export * from './types'
